472d6b047126d5c9db8804ee2550ee573f66d1df
Trend20 <enockomondi305@gmail.com>
2021-03-31 13:28:38 +0300

    fixing footer section

--- js/main.js
+++ js/main.js
@@ -32,3 +32,24 @@ function showContents() {
         }
     });
 }
+
+
+//Get the button:
+let myButton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        myButton.style.display = "block";
+    } else {
+        myButton.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
