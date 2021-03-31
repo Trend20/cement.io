737065cf52ca2f1a589d1df5134abf94d7fdbacc
Trend20 <enockomondi305@gmail.com>
2021-03-31 11:02:02 +0300

    fixing partners section

--- js/main.js
+++ js/main.js
@@ -32,20 +32,3 @@ function showContents() {
         }
     });
 }
-
-
-// sliders
-var indexValue = 0;
-
-function slideShow() {
-    setTimeout(slideShow, 2500);
-    var x;
-    const img = document.querySelectorAll("p-imgages");
-    for (x = 0; x < img.length; x++) {
-        img[x].style.display = "none";
-    }
-    indexValue++;
-    if (indexValue > img.length) { indexValue = 1 }
-    img[indexValue - 1].style.display = "block";
-}
-slideShow();
